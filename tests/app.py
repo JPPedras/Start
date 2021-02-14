88623673b547138357bc627af551300ce5e88f3c
João Pedras <joaoliveirapedras@gmail.com>
2021-02-14 14:18:48 +0000

    lol

--- tests/app.py
+++ tests/app.py
@@ -1,2 +1,6 @@
-print('hey')
-print('hey2')
+import random as rd
+import utils as ut
+
+numbers=[2, 5, 23, 5, 23, 55, 123, 1, 0]
+print(f'Max is {ut.find_max(numbers)}')
+
---
+++ tests/utils.py
@@ -0,0 +1,9 @@
+def find_max(list):
+
+    max=list[0]
+
+    for number in list:
+        if number>max:
+            max=number
+            
+    return max
